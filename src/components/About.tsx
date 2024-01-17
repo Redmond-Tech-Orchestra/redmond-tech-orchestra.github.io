f495aaaa29d1f8075b7eb92da02da1ce7bd5d4b9
Peter Yang <me@peteryang.io>
2024-01-17 00:04:02 -0800

    responsive about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,9 +13,13 @@ export default function About() {
                         nonprofit organization dedicated to fostering a vibrant cultural enviornment, supporting musical
                         education, and ensuring that the joy of orchestral performances is accessible to all.
                     </p>
+                    <p>
+                        We perform a wide variety of music, from classical to modern film scores. Our concerts
+                        are free and open to the public.
+                    </p>
                 </div>
                 <div>
-                    <img src={flute} alt="Musician playing a flute." width={450} />
+                    <img src={flute} alt="Musician playing a flute." />
                 </div>
             </div>
         </section>
